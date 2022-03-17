df2e7be9ae23fe0509a433c73a1b1d43381bd2ab
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-17 20:43:26 -0300

    inputs y select del modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -18,6 +18,36 @@ const Modal = ({ setShowModal, modalAnimation, setModalAnimation }) => {
       </div>
       <form className={`formulario ${modalAnimation ? "animar" : "cerrar"}`}>
         <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el Nombre del Gasto"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade el monto del Gasto: ejm 300"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoria">Categoría</label>
+          <select id="categoria">
+            <option value="">--Seleccione--</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="vivienda">Vivienda</option>
+            <option value="gastos varios">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+        <input type="submit" value="Añadir Gasto" />
       </form>
     </div>
   );
