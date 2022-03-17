b9a558bf84e96a57b25542ba1552ffdbc0b82de2
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-17 19:01:05 -0300

    añadiendo icono nuevo gasto y funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setShowModal(true);
+  };
 
   return (
     <div>
@@ -13,6 +19,16 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+      {showModal && <p>Desde modal</p>}
     </div>
   );
 }
