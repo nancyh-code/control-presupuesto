83debf4da9153e93b256b37dae991b03442835c8
Nancy Hernandez <hernandezyanezhotmail.com>
2022-03-17 15:56:12 -0300

    método para formatear cantidades

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
 const ControlPresupuesto = ({ presupuesto }) => {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -8,7 +15,7 @@ const ControlPresupuesto = ({ presupuesto }) => {
       </div>
       <div className="contenido-presupuesto">
         <p>
-          <span>Presupuesto: </span> {presupuesto}
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
         </p>
       </div>
     </div>
